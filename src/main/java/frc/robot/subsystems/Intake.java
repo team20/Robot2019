61a7c07aaed572890671499fa65e4f5cd3400607
Chrisvin123 <christophervin1@gmail.com>
2019-02-10 12:38:17 -0500

    Created intake

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+public class Intake {
+
+    private static Servo hatch;
+    private static VictorSPX cargo;
+
+    /**
+     * Initializes and sets up all motors for the intake
+     */
+    public Intake() {
+        hatch = new Servo(1);
+        cargo = new VictorSPX(0);
+    }
+
+    public void runCargo(double speed) {
+        cargo.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void openHatch() {
+        hatch.set(0);
+    }
+
+    public void closeHatch() {
+        hatch.set(1);
+    }
+}
