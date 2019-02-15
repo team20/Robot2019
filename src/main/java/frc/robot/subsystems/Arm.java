ae644d8553f020ca62cc28c6eb48bf840a42ceef
Sydney Walker <sydwal5434@gmail.com>
2019-02-15 18:49:15 -0500

    2/15 OperatorControls and Arm
     OperatorControls have been written
    
    Arm modified to include a method for each set position

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,8 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
@@ -10,56 +7,86 @@ import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Arm {
-    private static final int deviceID = 1;
-    private static CANSparkMax m_motor;
-    private static CANPIDController m_pidController;
-    private static CANEncoder m_encoder;
-    public static double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput,rotations;
+    private static CANSparkMax armMotor;
+    private static CANPIDController pidController;
+    private static CANEncoder armEncoder;
 
+    private static double setPosition, prevPosition;
+
+    private static final double DEADBAND = 0.3;
+
+    public static final double FLOOR_POSITION = 0.0;
+    public static final double CARGO_SHOOT_POSITION = 0.0;
+    public static final double PLACE_POSITION = 0.0;
+    public static final double STARTING_CONFIG_POSITION = 0.0;
+
+    /**
+     * Initializes all necessary objects and variables
+     */
     public Arm() {
-        m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);// initializes motor
-        kP = 0.0001;
-        kI = 0;
-        kD = 0;
-        kIz = 0;
-        kFF = 0;
-        /*
-The above code is just filler for the PID inputs until
-we actually get them down and are able to tune them 
-        */
-        kMaxOutput = 1;
-        kMinOutput = -1;
-        
-        m_pidController = m_motor.getPIDController();
-        m_encoder = m_motor.getEncoder();
-        //sends corresponding values to the pid controller object
-        m_pidController.setP(kP);
-        m_pidController.setI(kI);
-        m_pidController.setD(kD);
-        m_pidController.setIZone(kIz);
-        m_pidController.setFF(kFF);
-        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-     } 
-     /*
-     Takes in setpoint parameter 
-     A)Down
-     B)Up
-     C)Pre-Match
+        //motor setup
+        armMotor = new CANSparkMax(1, MotorType.kBrushless);
+        pidController = armMotor.getPIDController();
+        armEncoder = new CANEncoder(armMotor);
 
+        //initialize variables
+        setPosition = armEncoder.getPosition();
+        prevPosition = 0.0;
+
+        //sends corresponding values to the pid controller object
+        pidController.setP(0.001);
+        pidController.setI(0.0);
+        pidController.setD(0.0);
+        pidController.setOutputRange(-1.0, 1.0);
+     } 
+
+    /**
+     * Sets the arm to the floor position
      */
-     public static void setRevs(double rotation){
-rotations= rotation;
-     }
-     /*
-Set reference is similar to set poition. Refers to the setpoint
-Rotation is the setpoint, and controltype defines the type of output 
-     */
-     public static void Stablize(){
-        m_pidController.setReference(rotations, ControlType.kPosition);
-        SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());//returns encoder value
-        
-        
-        
+    public static void setStartingConfigPosition(){
+        setPosition(STARTING_CONFIG_POSITION);
     }
+
+    /**
+     * Sets the arm to the place (cargo or hatch) position
+     */
+    public static void setPlacePosition(){
+        setPosition(PLACE_POSITION);
+    }
+
+    /**
+     * Sets the arm to the cargo shoot position
+     */
+    public static void setCargoShootPosition(){
+        setPosition(CARGO_SHOOT_POSITION);
+    }
+
+    /**
+     * Sets the arm to the floor position
+     */
+    public static void setFloorPosition(){
+        setPosition(FLOOR_POSITION);
+    }
+
+    /**
+     * Sets the position of the elevator
+     * @param pos: desired position
+     */
+    public static void setPosition(double pos){
+        setPosition = pos;
+        pidController.setReference(setPosition, ControlType.kPosition);
+    }
+
+ 	/**
+	 * @return true if the arm is within deadband of its set position
+	 */
+	public static boolean armDoneMoving() {
+		if(Math.abs(armEncoder.getPosition() - prevPosition) > DEADBAND){
+			prevPosition = armEncoder.getPosition();
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
